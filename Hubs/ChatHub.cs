d97405d47ef2144008cb46dcbad283b61093f7f8
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-01-13 21:06:11 +0800

    写群组相关后端

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -3,15 +3,95 @@
 namespace SignalRWebpack.Hubs;
 
 public class ChatHub : Hub {
+	private readonly IMemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
+	private Group? jointGroup;
+	
+
 	public async Task NewMessage(string username, string message) =>
 		await Clients.All.SendAsync("messageReceived", username, message);
 
-	/*
-	public async Task JoinGroup(string name) {
+
+	public async Task JoinGroup(string name, string password) {
+		if (!memoryCache.TryGetValue<List<Group>>("ChatHub Groups", out var groups) || !groups!.Any(group => group.Name == name)) {
+			await Clients.Caller.SendAsync("groupResult", "joinFailed", "warning", "此群组不存在！");
+			return;
+		}
+		
+		
+		var group = groups!.First(group => group.Name == name);
+		if (!group.VerifyPassword(password)) {
+			await Clients.Caller.SendAsync("groupResult", "joinFailed", "error", "密码错误！");
+			return;
+		}
+
+		jointGroup = group;
+		lock (group) {
+			group.MemberCount++;
+		}
 		
 		await Groups.AddToGroupAsync(Context.ConnectionId, name);
 		await Clients.Caller.SendAsync("messageReceived", "Server", "You are in the group!");
-		
+		await Clients.OthersInGroup(name).SendAsync("messageReceived", "Server", $"{Context.ConnectionId} has joined the group!");
+
 	}
-	*/
+
+	public async Task LeaveGroup() {
+		if (jointGroup is null) {
+			await Clients.Caller.SendAsync("groupResult", "leaveFailed", "warning", "你没有加入任何群组！");
+			return;
+		}
+
+		lock (jointGroup) {
+			jointGroup.MemberCount--;
+			if (jointGroup.MemberCount == 0) {
+				var groups = memoryCache.Get<List<Group>>("ChatHub Groups")!;
+				lock (groups) {
+					groups.Remove(jointGroup);
+				}
+			}
+		}
+
+		await Clients.OthersInGroup(jointGroup.Name).SendAsync("messageReceived", "Server", $"{Context.ConnectionId} 已离开群组！");
+		await Groups.RemoveFromGroupAsync(Context.ConnectionId, jointGroup.Name);
+		await Clients.Caller.SendAsync("groupResult", "leaveSuccess", "success", "你已经成功离开群组！");
+
+		jointGroup = null;
+	}
+
+	public async Task CreateGroup(string name, string password) {
+		if (memoryCache.TryGetValue<List<Group>>("ChatHub Groups", out var groups) && groups!.Any(group => group.Name == name)) {
+			await Clients.Caller.SendAsync("groupResult", "createFailed", "warning", "此群组已存在！请更换群组名称！");
+			return;
+		}
+
+		var group = new Group(name, password);
+		if (groups is null) {
+			memoryCache.Set("ChatHub Groups", new List<Group> { group });
+		} else {
+			lock (groups) {
+				groups.Add(group);
+			}
+		}
+
+		await Groups.AddToGroupAsync(Context.ConnectionId, name);
+		await Clients.Caller.SendAsync("groupResult", "createSuccess", "success", "你已经成功创建群组！");
+	}
+
+	public override async Task OnDisconnectedAsync(Exception? exception) {
+		if (jointGroup is not null) {
+			lock (jointGroup) {
+				jointGroup.MemberCount--;
+				if (jointGroup.MemberCount == 0) {
+					var groups = memoryCache.Get<List<Group>>("ChatHub Groups")!;
+					lock (groups) {
+						groups.Remove(jointGroup);
+					}
+				}
+			}
+			jointGroup = null;
+		}
+		
+		await base.OnDisconnectedAsync(exception);
+	}
+
 }
--- Hubs/Group.cs
+++ Hubs/Group.cs
@@ -1,7 +1,62 @@
-﻿namespace SignalRWebpack.Hubs;
+﻿using System.Text;
+using System.Security.Cryptography;
 
-internal struct Group {
-	public string Name { get; set; }
-	public byte[] PasswordSalt { get; set; }
-	public byte[] PasswordHash { get; set; }
+namespace SignalRWebpack.Hubs;
+
+internal class Group {
+	private const int SaltLength = 16;
+	private const int HashLength = 256;
+
+	/// <summary>
+	/// 群组成员数量
+	/// </summary>
+	public ulong MemberCount { get; set; } = 0;
+	
+	/// <summary>
+	/// 群组名称
+	/// </summary>
+	public string Name { get; init; }
+	
+	/// <summary>
+	/// 群组密码的盐
+	/// </summary>
+	public byte[] PasswordSalt { get; private init; }
+
+	/// <summary>
+	/// 群组密码的哈希值
+	/// </summary>
+	public byte[] PasswordHash { get; private init; }
+
+	/// <summary>
+	/// 验证给定的密码是否与当前群组的密码相匹配。
+	/// </summary>
+	/// <param name="password">给定的密码</param>
+	/// <returns>若匹配，则为 <see cref="true"/>；否则为 <see cref="false"/>。</returns>
+	public bool VerifyPassword(string password) {
+		var computeResult = new byte[HashLength];
+		var passwordData = Encoding.Unicode.GetBytes(password);
+		var combinedData = new byte[passwordData.Length + PasswordSalt.Length];
+		Buffer.BlockCopy(passwordData, 0, combinedData, 0, passwordData.Length);
+		Buffer.BlockCopy(PasswordSalt, 0, combinedData, passwordData.Length, PasswordSalt.Length);
+		SHA256.HashData(combinedData, computeResult);
+		return PasswordHash.SequenceEqual(computeResult);
+	}
+
+	/// <summary>
+	/// 创建一个新的群组。
+	/// </summary>
+	/// <param name="name">群组名称</param>
+	/// <param name="password">群组密码</param>
+	public Group(string name, string password) {
+		Name = name;
+		PasswordSalt = new byte[SaltLength];
+		PasswordHash = new byte[HashLength];
+		using var rng = RandomNumberGenerator.Create();
+		rng.GetBytes(PasswordSalt);
+		var passwordData = Encoding.Unicode.GetBytes(password);
+		var combinedData = new byte[passwordData.Length + PasswordSalt.Length];
+		Buffer.BlockCopy(passwordData, 0, combinedData, 0, passwordData.Length);
+		Buffer.BlockCopy(PasswordSalt, 0, combinedData, passwordData.Length, PasswordSalt.Length);
+		SHA256.HashData(combinedData, PasswordHash);
+	}
 }
