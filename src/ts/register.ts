8c760b13c1faabc3866b1f35faa3471a3469042e
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-07-25 00:30:01 +0800

    修复注册页暂不可用弹窗 Footer 的 Anchor 无法使用的问题

--- src/ts/register.ts
+++ src/ts/register.ts
@@ -3,23 +3,24 @@ import Swal from 'sweetalert2/dist/sweetalert2.min.js';
 import '../css/login.css';
 
 document.querySelector('html').addEventListener('click', e => {
-	e.preventDefault();
-	Swal.fire({
-		title: '这里啥也没有',
-		html: '请联系 GitHub @execute233 早日完成！',
-		icon: 'question',
-		footer: '<a href="https://github.com/execute233">GitHub @execute233</a>'
-	});
+	const target = e.target as HTMLElement;
+	if (!(target instanceof HTMLAnchorElement && target.href?.endsWith("execute233"))) {
+		e.preventDefault();
+		Swal.fire({
+			title: '这里啥也没有',
+			text: '请联系 GitHub @execute233 早日完成！',
+			icon: 'question',
+			footer: '<a href="https://github.com/execute233" target="_blank">GitHub @execute233</a>'
+		});
+	}
 });
 
 
 /*
 To @execute233:
 
-我打算采用邀请注册制。
-你觉得后台生成邀请码，丢给用户注册好；
-还是后台生成 ID 和临时密码，丢给用户注册好；
-还是用户直接注册，然后服务器生成一段文本，复制后丢给管理员，管理员后台审核；
-抑或是不允许注册，由管理员直接在后台添加用户？
+用户直接注册，然后服务器生成一段文本，复制后丢给管理员，管理员后台审核。
+实现方法可以参照原 CZCA 曾经的网站的代码。
+快写吧😊
 
 */
