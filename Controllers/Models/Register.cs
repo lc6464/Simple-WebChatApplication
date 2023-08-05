49e2319e4b80ee1bbab72964d1677a346cc31803
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-08-05 22:26:29 +0800

    写解密写傻了 @execute233 我到底哪里错了呢？

--- Controllers/Models/Register.cs
+++ Controllers/Models/Register.cs
@@ -23,5 +23,45 @@ public readonly struct RegisterUserData {
 	public byte[]? PasswordSalt { get; init; }
 
 	[JsonPropertyName("k")]
-	public readonly byte[] HMACKey = ICheckingTools.GenerateRandomData(16).ToArray();
+	public byte[] HMACKey => ICheckingTools.GenerateRandomData(16).ToArray();
+}
+
+
+
+public readonly struct RegisterGet {
+	public bool Success { get; init; }
+	public int Code { get; init; }
+	public string? Message { get; init; }
+	public RegisterUserDataOutput? Data { get; init; }
+}
+
+public readonly struct RegisterUserDataGet {
+	[JsonPropertyName("a")]
+	public string? Account { get; init; }
+
+	[JsonPropertyName("h")]
+	public byte[]? PasswordHash { get; init; }
+
+	[JsonPropertyName("s")]
+	public byte[]? PasswordSalt { get; init; }
+
+	[JsonPropertyName("k")]
+	public byte[]? HMACKey { get; init; }
+}
+
+public readonly struct RegisterUserDataOutput {
+	public RegisterUserDataOutput(RegisterUserDataGet userData, long timestamp) {
+		Account = userData.Account;
+		PasswordHash = userData.PasswordHash;
+		PasswordSalt = userData.PasswordSalt;
+		Timestamp = timestamp;
+	}
+
+	public string? Account { get; init; }
+
+	public byte[]? PasswordHash { get; init; }
+
+	public byte[]? PasswordSalt { get; init; }
+
+	public long? Timestamp { get; init; }
 }
--- Services/EncryptionTools.cs
+++ Services/EncryptionTools.cs
@@ -9,9 +9,13 @@ namespace SimpleWebChatApplication.Services;
 /// </summary>
 public class EncryptionTools : IEncryptionTools {
 	private readonly IDataProvider _provider;
+	private readonly ILogger<EncryptionTools> _logger;
 	//private readonly ICheckingTools _tools;
 
-	public EncryptionTools(IDataProvider provider) => _provider = provider;
+	public EncryptionTools(IDataProvider provider, ILogger<EncryptionTools> logger) {
+		_provider = provider;
+		_logger = logger;
+	}
 
 	/*
 	public EncryptionTools(IDataProvider provider, ICheckingTools tools) {
@@ -26,18 +30,51 @@ public class EncryptionTools : IEncryptionTools {
 	/// <param name="userData">待加密的 <see cref="RegisterUserData"/></param>
 	/// <returns>加密后的结果</returns>
 	public string EncryptUserData(RegisterUserData userData) {
-		var data = JsonSerializer.SerializeToUtf8Bytes(userData); // 序列化 UserData
+		var utf8UserData = JsonSerializer.SerializeToUtf8Bytes(userData); // 序列化 UserData
 		var timestamp = BitConverter.GetBytes(new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds()); // 获取当前时间戳数据
-		var signRawData = new byte[timestamp.Length + data.Length]; // 创建签名前的原始数据
+		var signRawData = new byte[timestamp.Length + utf8UserData.Length]; // 创建签名前的原始数据
 		Buffer.BlockCopy(timestamp, 0, signRawData, 0, timestamp.Length); // 将时间戳数据复制到原始数据中
-		Buffer.BlockCopy(data, 0, signRawData, timestamp.Length, data.Length); // 将 UserData Utf8Bytes 复制到原始数据中
+		Buffer.BlockCopy(utf8UserData, 0, signRawData, timestamp.Length, utf8UserData.Length); // 将 UserData Utf8Bytes 复制到原始数据中
 		using HMACSHA256 sha256 = new(userData.HMACKey); // 创建 HMACSHA256 对象，Key 随机生成
 		var signData = sha256.ComputeHash(signRawData); // 计算签名数据
 		var signatureRawData = new byte[signData.Length + timestamp.Length]; // 创建签名数据
 		Buffer.BlockCopy(signData, 0, signatureRawData, 0, signData.Length); // 将签名结果复制到签名数据中
 		Buffer.BlockCopy(timestamp, 0, signatureRawData, signData.Length, timestamp.Length); // 将时间戳数据复制到签名数据中
-		var encryptedDataString = Base64UrlTextEncoder.Encode(IEncryptionTools.Encrypt256(data, _provider.RegisterEncryptionKey, _provider.RegisterEncryptionIV));
+		var encryptedDataString = Base64UrlTextEncoder.Encode(IEncryptionTools.Encrypt256(utf8UserData, _provider.RegisterEncryptionKey, _provider.RegisterEncryptionIV));
 		var signature = Base64UrlTextEncoder.Encode(signatureRawData);
 		return $"{encryptedDataString}/{signature}"; // 返回加密后的数据
+	}
+
+	/// <summary>
+	/// 解密注册数据。
+	/// </summary>
+	/// <param name="input">待解密的字符串</param>
+	/// <returns>解密后的数据</returns>
+	public bool TryDecryptUserData(string[] input, out RegisterUserDataOutput? output) {
+		var signaturePart = Base64UrlTextEncoder.Decode(input[1]); // 签名部分
+		var signature = signaturePart[..^8]; // 签名
+		var timestamp = signaturePart[^8..]; // 时间戳
+		//_logger.LogDebug("sign: {}", Convert.ToBase64String(signature));
+		//_logger.LogDebug("ts: {}", BitConverter.ToInt64(timestamp).ToString());
+
+		var utf8UserData = IEncryptionTools.Decrypt256(Base64UrlTextEncoder.Decode(input[0]),
+			_provider.RegisterEncryptionKey, _provider.RegisterEncryptionIV); // 解密后的 UserData
+
+		var userData = JsonSerializer.Deserialize<RegisterUserDataGet>(utf8UserData); // 反序列化 UserData
+		//_logger.LogDebug("UserData: {}", JsonSerializer.Serialize(userData));
+
+		var signRawData = new byte[timestamp.Length + utf8UserData.Length]; // 创建签名前的原始数据
+		Buffer.BlockCopy(timestamp, 0, signRawData, 0, timestamp.Length); // 将时间戳数据复制到原始数据中
+		Buffer.BlockCopy(utf8UserData, 0, signRawData, timestamp.Length, utf8UserData.Length); // 将 UserData Utf8Bytes 复制到原始数据中
+		using HMACSHA256 sha256 = new(userData.HMACKey!); // 使用传入的 Key 创建 HMACSHA256 对象
+		var signData = sha256.ComputeHash(signRawData); // 计算签名数据
+		//_logger.LogDebug("signData: {}", Convert.ToBase64String(signData));
+
+		//if (signData.SequenceEqual(signature)) { // 比较签名数据
+			output = new RegisterUserDataOutput(userData, BitConverter.ToInt64(timestamp)); // 返回解密后的数据
+			return true;
+		//}
+		//output = null;
+		//return false;
 	}
 }
