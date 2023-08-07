d5b5b2fe25b73e8bbb13218930c2a282deef1564
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-08-07 10:59:18 +0800

    暂存代码以供分析

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -16,7 +16,9 @@ public class RegisterController : ControllerBase {
 
 	[HttpGet]
 	[ResponseCache(CacheProfileName = "NoStore")]
-	public RegisterGetResponse Get([FromForm(Name = "access-token")] string? accessToken, [FromForm(Name = "register-data")] string? registerData) {
+	public RegisterGetResponse Get([FromForm(Name = "access-token")] string? accessToken,
+		[FromForm(Name = "register-data")] string? registerData) {
+
 		if (!string.IsNullOrWhiteSpace(accessToken) && HttpContext.Session.GetString("ManageAccessToken") == accessToken) {
 			return new() { Code = 6, Success = false, Message = "未登录管理后台。" };
 		}
@@ -39,7 +41,9 @@ public class RegisterController : ControllerBase {
 
 	[HttpPost]
 	[ResponseCache(CacheProfileName = "NoStore")]
-	public RegisterUserPostResponse UserPost([FromForm] string? account, [FromForm] string? password, [FromForm(Name = "repeat-password")] string? repeatPassword) {
+	public RegisterUserPostResponse UserPost([FromForm] string? account, [FromForm] string? password,
+		[FromForm(Name = "repeat-password")] string? repeatPassword) {
+
 		if (_tools.IsLogin()) {
 			return new() { Success = false, Code = 2, Message = "您当前已登录。" };
 		}
@@ -50,7 +54,8 @@ public class RegisterController : ControllerBase {
 			return new() { Success = false, Code = 4, Message = "两次输入的密码不一致。" };
 		}
 		if (!ICheckingTools.IsPasswordComplicated(password)) {
-			return new() { Success = false, Code = 8, Message = "密码复杂度不足（最少10位且须足够复杂）或过长（最多64位）。" };
+			return new() { Success = false, Code = 8, Message = "密码复杂度不足（最少10位且包含数字、大写字母、小写字母、特殊符号中" +
+				"至少三种且每种超过两个）或过长（最多64位）。" };
 		}
 		if (!_tools.IsNameAvailable(account)) {
 			return new() { Success = false, Code = 9, Message = "用户名不符合要求或已被占用（4~32位，字母开头，允许数字、字母、下划线、减号）。" };
@@ -72,8 +77,10 @@ public class RegisterController : ControllerBase {
 
 	[HttpPost(Name = "import")]
 	[ResponseCache(CacheProfileName = "NoStore")]
-	public dynamic? Import([FromForm(Name = "access-token")] string? accessToken, [FromForm(Name = "register-data")] string? registerData) {
-		/*if (!string.IsNullOrWhiteSpace(accessToken) && HttpContext.Session.GetString("ManageAccessToken") == accessToken) {
+	public RegisterImportResponse Import([FromForm(Name = "access-token")] string? accessToken,
+		[FromForm(Name = "register-data")] string? registerData) {
+
+		if (!string.IsNullOrWhiteSpace(accessToken) && HttpContext.Session.GetString("ManageAccessToken") == accessToken) {
 			return new() { Code = 6, Success = false, Message = "未登录管理后台。" };
 		}
 		if (string.IsNullOrWhiteSpace(registerData)) {
@@ -84,12 +91,16 @@ public class RegisterController : ControllerBase {
 			return new() { Code = 4, Success = false, Message = "无法解析注册数据。" };
 		}
 		try {
-			return _encryptionTools.TryDecryptUserData(registerParts, out var output)
-				? new() { Code = 0, Success = true, Data = output }
-				: new() { Code = 4, Success = false, Message = "已解析数据，但签名验证失败！" };
+			if (!_encryptionTools.TryDecryptUserData(registerParts, out var output)) {
+				return new() { Code = 4, Success = false, Message = "已解析数据，但签名验证失败！" };
+			}
+			if (!_tools.IsNameAvailable(output?.Account!)) {
+				return new() { Code = 9, Success = false, Message = "用户名不符合要求或已被占用（4~32位，字母开头，允许数字、字母、下划线、减号）。" };
+			}
+
+			return new() { Code = 0, Success = true, /*Data = output*/ };
 		} catch (Exception e) {
 			return new() { Code = 4, Success = false, Message = $"无法解析注册数据：{e}" };
-		}*/
-		return null;
+		}
 	}
 }
