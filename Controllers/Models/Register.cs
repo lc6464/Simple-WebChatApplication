294b1b1f2c06b01f383ff6683b70bf3f26b81f53
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-11-30 15:29:29 +0800

    删除未使用的 Using

--- Controllers/Models/Register.cs
+++ Controllers/Models/Register.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using SimpleWebChatApplication.Services;
 
 namespace SimpleWebChatApplication.Controllers.Models;
